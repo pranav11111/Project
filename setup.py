44a81fe1d465af582a1798f7f54157425077edf5
Pranav <pranavp1712@gmail.com>
2025-03-18 12:36:43 +0530

    Setup.py complete

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import find_packages, setup
-from  typing import List
-
+from typing import List
 
 e_dot = '-e .'
 
@@ -10,21 +9,21 @@ def get_req(filename: str)-> List[str]:
     '''
 
     requirements = []
-    with open(filename) as fileobj:
-        requirements = fileobj.readlines()
-        requirements = [req.replace('\n','') for req in requirements]
+    with open(filename) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
 
         if e_dot in requirements:
-            requirements.remove('-e .')
+            requirements.remove(e_dot)
 
     return requirements
 
 
 setup(
-name= "Mantainance Prediciton"
-version= "0.0.1"
-author= "Pranav"
-author_email= "pranavp1712@gmail.com"
-packages= find_packages()
+name = "Mantainance_Prediciton",
+version = "0.0.1",
+author = "Pranav",
+author_email = "pranavp1712@gmail.com",
+packages = find_packages(),
 install_requires = get_req('requirements.txt')
 )
