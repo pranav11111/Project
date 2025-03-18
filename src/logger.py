da36821a019b72c3a925c88430f63489d4a370c3
Pranav <pranavp1712@gmail.com>
2025-03-18 13:57:35 +0530

    Basic structure creation along with logger and exception

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), 'Logs', log_file)
+os.makedirs(logs_path, exist_ok= True)
+
+
+logfile_path = os.path.join(logs_path,log_file)
+
+logging.basicConfig(
+filename= logfile_path,
+format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+level = logging.INFO,
+
+)
+
+if __name__ == '__main__':
+    logging.info("Loggin has started")
